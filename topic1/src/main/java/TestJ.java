21a75e39a0a9a20120e5c4e09308251514602435
Dave <davidjupg@gmail.com>
2018-05-07 17:29:41 -0300

    minor changes, just re-doing code for better understanding

--- topic1/src/main/java/TestJ.java
+++ topic1/src/main/java/TestJ.java
@@ -1,12 +1,9 @@
 import org.testng.annotations.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestJ {
 
     @Test
-
     public void test() {
         String str = "It is working!";
         assertEquals("It is working!", str);

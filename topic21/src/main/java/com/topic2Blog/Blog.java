d33a68031df50f03455ff0aa15343831fcb139fe
Dave <davidjupg@gmail.com>
2018-05-12 02:15:23 -0300

    Done with commenting topic 2 Blog part... 88% coverage on tests

--- topic21/src/main/java/com/topic2Blog/Blog.java
+++ topic21/src/main/java/com/topic2Blog/Blog.java
@@ -8,30 +8,41 @@ import java.util.Iterator;
 
 public class Blog {
 
+    ///Here goes my Blog
 
     @Getter
     @Setter
     private ArrayDeque<Post> bloggy;
 
+
+
     public Blog(){
         this.bloggy = new ArrayDeque<Post>();
     }
 
+
+    ///Self explanatory
     public Boolean isEmpty(){
         return this.bloggy.isEmpty();
     }
+
+    ///Adds a new Post on top of the ArrayDeque
     public void addNewPost(Post newP){
         this.bloggy.addFirst(newP);
     }
 
+    ///Returns the newest Post Created
     public Post getNewestPost(){
         return this.bloggy.getFirst();
     }
 
+    ///Gives us the latest post.
     public Post getOldestPost(){
         return this.bloggy.getLast();
     }
 
+    ///Looks for the Title in ArrayDeque and vanishes it into the sky
+    ///Returns True if it is found
     public Boolean deletePostByTitle(String titlePost){
         Boolean answer = false;
         for (Post searching: this.bloggy) {
@@ -43,6 +54,7 @@ public class Blog {
         return answer;
     }
 
+    ///Creates and returns a new ArrayDeque with the lastest 10 posts
     public ArrayDeque getNewestTenPosts(){
         ArrayDeque<Post> newestPosts = new ArrayDeque<>();
         Iterator ite = this.bloggy.iterator();
--- topic21/src/test/java/com/topic2Blog/BlogTest.java
+++ topic21/src/test/java/com/topic2Blog/BlogTest.java
@@ -14,10 +14,12 @@ public class BlogTest {
 
     private Blog postsList;
 
+    ///Let's set up everything before we do anything
     @Before
     public void setUp(){this.postsList = new Blog();}
 
 
+    ///Comparing using addNewPost() which inserts in the first place of the ArrayDeque with Mocking a new Post
     @Test
     public void addNewPost() {
         Post post = mock(Post.class);
@@ -27,12 +29,14 @@ public class BlogTest {
         assertEquals(this.postsList.getNewestPost().getTitle(), post.getTitle());
     }
 
+    ///Testing testing if it's empty
     @Test
     public void PostListIsEmpty() {
         assertTrue(this.postsList.isEmpty());
     }
 
 
+    ///Using the method deletePostByTitle() which returns true or false on a blog, with mocking posts
     @Test
     public void deletePostByName() {
         Boolean answer = false;
@@ -50,7 +54,7 @@ public class BlogTest {
         for (Post posty : this.postsList.getBloggy()) {
 
             if (postX.getTitle().equalsIgnoreCase(posty.getTitle())) {
-                this.postsList.getBloggy().remove(posty);
+                this.postsList.deletePostByTitle(posty.getTitle());
                 answer = true;
             }
 
@@ -58,6 +62,8 @@ public class BlogTest {
         assertTrue(answer);
     }
 
+    ///getNewestTenPosts() returns a new ArrayDeque with the latest 10 posts
+    ///comparing the size of the new ArrayDeque "lastTen" with 10
     @Test
     public void bringMeTheTenMostRecent(){
         Post postD = mock(Post.class);
@@ -67,5 +73,24 @@ public class BlogTest {
         }
         ArrayDeque<Post> lastTen = this.postsList.getNewestTenPosts();
         assertEquals(lastTen.size(),10);
+    }
+
+    ///Don't know why using it again let's just leave it here commented
+    /*
+    @Test
+    public void isEmpty(){
+        assertTrue(this.postsList.isEmpty());
+    }
+    */
+
+    ///Making sure that the method getOldestPost() returns the latest post using mocking post on my blog.
+    @Test
+    public void oldestPost(){
+        Post postD = mock(Post.class);
+        for (int i=0;i < 15; i++){
+            when(postD.getTitle()).thenReturn("Mocking Post " + i);
+            this.postsList.getBloggy().add(postD);
+        }
+        assertEquals("Mocking Post 14",this.postsList.getOldestPost().getTitle());
     }
 }

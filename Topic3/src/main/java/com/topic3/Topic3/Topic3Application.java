412fdb33cca134e8ec1bfbf7d66e1eb4e68c94f1
Dave <davidjupg@gmail.com>
2018-05-14 14:14:06 -0300

    Topic 3 init

---
+++ Topic3/src/main/java/com/topic3/Topic3/Topic3Application.java
@@ -0,0 +1,12 @@
+package com.topic3.Topic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic3Application.class, args);
+	}
+}

f250ee617655a051df5c5feead1bd58d231cd724
Dave <davidjupg@gmail.com>
2018-05-27 12:29:39 -0300

    pending user repository

---
+++ api/src/main/java/com/bootcamp/api/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.bootcamp.api.repositories;
+
+
+import com.bootcamp.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User,Long> {
+    User findByEmail (@Param("email")String email);
+    User findByToken(String token);
+    boolean existsByEmail (@Param("email")String email);
+}
